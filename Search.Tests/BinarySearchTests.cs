bf259c546e0b807d1c64f791fd041654ba989ef3
phoenixIS <savchira9@gmail.com>
2017-07-25 06:42:49 +0300

    Binary Search implemented

---
+++ Search.Tests/BinarySearchTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Search.Tests
+{
+    [TestFixture]
+    public class BinarySearchTests
+    {
+        [TestCase(new[] { -3, 2, 8, 11, 19, 21 }, 11, ExpectedResult = 3)]
+        [TestCase(new[] { -15, -4, 5, 9, 18, 20 }, 20, ExpectedResult = 5)]
+        [TestCase(new[] { -18, -15, -13, 0, 4, 9, 10 }, 5, ExpectedResult = -1)]
+        public int BinarySearchTest_IntArray_ItemIndex(int[] array, int item)
+        {
+            return BinarySearch.Search(array, item, Comparer<int>.Default);
+        }
+
+        [TestCase(new[] { -3.3, 2.5, 8.9, 11.1, 19.2, 21.4 }, 11.1, ExpectedResult = 3)]
+        [TestCase(new[] { -15.6, -4.1, 5.3, 9.6, 18.2, 20.4 }, 20.4, ExpectedResult = 5)]
+        [TestCase(new[] { -18.8, -15.1, -13.2, 0, 4.3, 9.2, 10.9 }, 5.8, ExpectedResult = -1)]
+        public int BinarySearchTest_DoubleArray_ItemIndex(double[] array, double item)
+        {
+            return BinarySearch.Search(array, item, Comparer<double>.Default);
+        }
+
+        [TestCase(new[] { "Abraham", "Barbara", "Michael", "Phillip" }, "Phillip", ExpectedResult = 3)]
+        [TestCase(new[] { "David", "Frank", "George" }, "George", ExpectedResult = 2)]
+        [TestCase(new[] { "Aria", "Nick", "Thomas" }, "Lily", ExpectedResult = -1)]
+        public int BinarySearchTest_StringArray_ItemIndex(string[] array, string item)
+        {
+            return BinarySearch.Search(array, item, Comparer<string>.Default);
+        }
+
+        [TestCase(null, 2)]
+        public void BinarySearchTest_Null_ThrowsArgumentNullException(int[] array, int item)
+        {
+            Assert.Throws<ArgumentNullException>(() => BinarySearch.Search(array, item, Comparer<int>.Default));
+        }
+
+        [TestCase(new int[] { }, 9)]
+        public void BinarySearchTest_EmptyArray_ThrowsArgumentException(int[] array, int item)
+        {
+            Assert.Throws<ArgumentException>(() => BinarySearch.Search(array, item, Comparer<int>.Default));
+        }
+    }
+}
---
+++ Search/BinarySearch.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Search
+{
+    /// <summary>
+    /// Class containing the Binary Search algorithm logic
+    /// </summary>
+    public static class BinarySearch
+    {
+        private static int NoMatchFound = -1;
+
+        /// <summary>
+        /// Searches an item in the array using the Binary Search algorithm
+        /// </summary>
+        /// <typeparam name="T">Type of items in the array</typeparam>
+        /// <param name="array">Sorted array to search the item in</param>
+        /// <param name="item">Item to be found</param>
+        /// <param name="comparer">Comparer representing sorting criteria</param>
+        /// <returns>Index of the item in the array or -1 if it was not found</returns>
+        public static int Search<T>(T[] array, T item, IComparer<T> comparer)
+        {
+            CheckInputArray(array);
+            return PerformSearch(array, item, comparer);
+        }
+
+        private static int PerformSearch<T>(T[] array, T item, IComparer<T> comparer)
+        {
+            if (array.Length == 0)
+                return NoMatchFound;
+
+            int middle = array.Length / 2;
+
+            int comparisonResult = comparer.Compare(array[middle], item);
+
+            if (comparisonResult == 0)
+                return middle;
+            if (comparisonResult > 0)
+                return PerformSearch<T>(GetArrayPart(array, 0, middle - 1), item, comparer);
+
+            int index = PerformSearch<T>(GetArrayPart(array, middle + 1, array.Length - 1), item, comparer);
+            return index == NoMatchFound ? NoMatchFound : middle + index + 1;
+        }
+
+        private static T[] GetArrayPart<T>(T[] array, int first, int last)
+        {
+            List<T> newArray = new List<T>();
+
+            for (int i = first; i <= last; i++)
+                newArray.Add(array[i]);
+
+            return newArray.ToArray();
+        }
+
+        private static void CheckInputArray<T>(T[] array)
+        {
+            if (array == null)
+                throw new ArgumentNullException($"{nameof(array)} cannot be null");
+            if (array.Length == 0)
+                throw new ArgumentException($"{nameof(array)} cannot be empty");
+        }
+    }
+}
